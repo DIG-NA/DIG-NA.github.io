390c315b96e12aa5e217b39221afb5b016f3a10b
ELMUTAZ-ABDELRAHMAN <xoyenterprise@gmail.com>
2025-05-16 15:50:54 +0400

    hopefully it works now

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "574204658"
+    serviceWorkerVersion: "1097895711"
   }
 });
